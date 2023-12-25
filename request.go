ba3e27c985af0ffbc1f82cfbe8e1e500cf15759b
Severuykhin Igor <igor@collabox.dev>
2023-12-25 13:33:40 +0300

    feat: Removed dependency on the goerrors package when parsing body

--- request.go
+++ request.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/severuykhin/goerrors"
 )
 
 type Request struct {
@@ -15,17 +14,16 @@ type Request struct {
 func (c *Request) BodyParser(dest interface{}) error {
 	body := c.Body()
 	if len(body) == 0 {
-		return goerrors.NewBadRequestErr().WithMessage("empty body")
+		return fmt.Errorf("empty body")
 	}
 
 	err := json.Unmarshal(body, dest)
 	if err != nil {
 		switch err := err.(type) {
 		case *json.UnmarshalTypeError:
-			return goerrors.NewBadRequestErr().
-				WithMessage(fmt.Sprintf("field:%s must be of type:%s instead has type:%s", err.Field, err.Type, err.Value))
+			return fmt.Errorf("field:%s must be of type:%s instead has type:%s", err.Field, err.Type, err.Value)
 		default:
-			return goerrors.NewBadRequestErr().WithMessage(err.Error())
+			return err
 		}
 	}
 	return nil
